5868ac1f198fd020dac76c297ae4282ab05ea14a
Varun Mhatre <varunjmhatre@gmail.com>
2020-12-31 02:10:09 -0500

    Updated premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,8 @@
 project "ImGui"
   kind "StaticLib"
   language "C++"
+  cppdialect "C++17"
+  staticruntime "on"
   
   targetdir ("bin/" .. outputdir .. "/%{prj.name}")
   objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -12,6 +14,7 @@ project "ImGui"
     "imgui.cpp",
     "imgui_draw.cpp",
     "imgui_internal.h",
+    "imgui_tables.cpp",
     "imgui_widgets.cpp",
     "imstb_rectpack.h",
     "imstb_textedit.h",
@@ -21,19 +24,15 @@ project "ImGui"
   
   filter "system:windows"
     systemversion "latest"
-    cppdialect "C++17"
-    staticruntime "On"
     
   filter "system:linux"
-    pic "On"
+    pic "on"
     systemversion "latest"
-    cppdialect "C++17"
-    staticruntime "On"
     
-  filter "configurations:debug"
+  filter "configurations:Debug"
     runtime "Debug"
     symbols "on"
     
-  filter "configurations:release"
+  filter "configurations:Release"
     runtime "Release"
     optimize "on"
